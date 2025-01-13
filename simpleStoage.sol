2474acba8ca45e1699091db4396cbb82c6c9cf3e
Deep912 <deepchhegaliya@gmail.com>
2025-01-13 16:50:54 +0530

    added mapping technique in simpleStorage

--- simpleStoage.sol
+++ simpleStoage.sol
@@ -4,18 +4,22 @@ pragma solidity ^0.8.26;
 contract simple{
 // struct is used to define custom type
 struct Person{
-    uint256 number;
     string name;
+    uint256 number;
+    
 }
 
 //we are making array of Person(struct) aka dynamic array
 Person[] public listOfPeople;
 
+//mapping a value to another value for ex string => uint256 = deep => 10 so string "deep" is now mapped with uint256 10 number
+mapping (string => uint256) public nameToNumber;
 //static array
 // Person[3] public listOfPeople;
 
-function addPeople(uint256 _number,string memory _name) public {
-    listOfPeople.push(Person(_number, _name)); //pushing new people in Person
+function addPeople(string memory _name,uint256 _number) public {
+    listOfPeople.push(Person( _name ,_number)); //pushing new people in Person
+    nameToNumber[_name] = _number; // we added function that will map name with number so deep to 10
 }
 
 // Person public myFriend = Person({ number : 7 , name: "pet"});
