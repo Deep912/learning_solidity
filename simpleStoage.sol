f9e1775e87cd7a469fc68931f26b2b428126eef2
Deep912 <deepchhegaliya@gmail.com>
2025-01-13 13:05:10 +0530

    adding new data in array and retriving data with the help of .push function

--- simpleStoage.sol
+++ simpleStoage.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.26;
 
 contract simple{
-
+// struct is used to define custom type
 struct Person{
     uint256 number;
     string name;
 }
 
-Person public myFriend = Person({ number : 7 , name: "pet"});
+//we are making array of Person(struct) aka dynamic array
+Person[] public listOfPeople;
+
+//static array
+// Person[3] public listOfPeople;
+
+function addPeople(uint256 _number,string memory _name) public {
+    listOfPeople.push(Person(_number, _name)); //pushing new people in Person
+}
+
+// Person public myFriend = Person({ number : 7 , name: "pet"});
 
 }
