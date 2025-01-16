a9a046503583bf82ff0fa57bea75b52bb21b0d12
Deep912 <deepchhegaliya@gmail.com>
2025-01-16 12:02:38 +0530

    we got price using chainlink but we are getting value with 18 decimals so we calculated and also typecasted it in to uint256 cuz that price data we received was in int256

--- S_101_p2/FundME.sol
+++ S_101_p2/FundME.sol
@@ -1,22 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
+//importing interface from github
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-  function description() external view returns (string memory);
+//either we copy whole code and past it like this to use or we can directly import it from github as shown above
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
 
-  function version() external view returns (uint256);
+//   function description() external view returns (string memory);
 
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+//   function version() external view returns (uint256);
 
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+//   function getRoundData(
+//     uint80 _roundId
+//   ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+//   function latestRoundData()
+//     external
+//     view
+//     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+// }
 
 contract FundMe{
     
@@ -28,11 +32,13 @@ contract FundMe{
         require(msg.value >= 1e18, "didnt send enough RTH ");// 1e18 = 1 ETH = 
 
     }
-    function getPrice() public {
+    function getPrice() public view returns (uint256){
         //address 0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
         //abi 
-        // AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22).version();
-    }
+        AggregatorV3Interface PriceFeed = AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22);
+        (,int256 price ,,,) = PriceFeed.latestRoundData(); //we will get a 18 decimal numgber by this so we have to convert it and also typecast
+        return uint256(price * 1e10);
+   }
     function getConversionRate() public{}
 
     function getVersion() public view returns(uint256){
