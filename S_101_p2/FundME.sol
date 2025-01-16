c5691f001e2723042ea0dc8c6dadd5e952924641
Deep912 <deepchhegaliya@gmail.com>
2025-01-16 11:41:31 +0530

    getting data from outer sources using chainlink

--- S_101_p2/FundME.sol
+++ S_101_p2/FundME.sol
@@ -1,8 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract FundMe{
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
 
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+contract FundMe{
+    
     uint256 public minimumUsd = 5;
     function fund() public payable {
         //allowing users to send $
@@ -10,5 +27,15 @@ contract FundMe{
         //1. how do we sned ETH to this contract?
         require(msg.value >= 1e18, "didnt send enough RTH ");// 1e18 = 1 ETH = 
 
+    }
+    function getPrice() public {
+        //address 0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
+        //abi 
+        // AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22).version();
+    }
+    function getConversionRate() public{}
+
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22).version();
     }
 }
